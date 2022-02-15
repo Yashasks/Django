fac7933df52d2a6103f7f1758d5122db83c2bdeb
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-02-15 18:11:28 +0530

    demp-project-django

--- demoproject/demoproject/urls.py
+++ demoproject/demoproject/urls.py
@@ -1,11 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about, name='about-page'),
-    path('', views.home, name='home')
-    
+    path('', views.home, name='home'),
+    path('articles/',include('articles.urls')),
 ]
--- demoproject/demoproject/views.py
+++ demoproject/demoproject/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def about(request):
-    return HttpResponse('about')
+    return render(request, 'about.html')
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'home.html')
