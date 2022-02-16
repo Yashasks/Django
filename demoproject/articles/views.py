8d13a4fc079edd610c0d3fc7c759191715ac7242
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-02-16 23:34:05 +0530

    django-demo

--- demoproject/articles/views.py
+++ demoproject/articles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def article_list(request):
-    return render(request,'articles/article_list.html')
+    article = Article.objects.all().order_by('name')
+    
+    return render(request,'articles/article_list.html', {'article':article})
--- demoproject/demoproject/urls.py
+++ demoproject/demoproject/urls.py
@@ -2,10 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', views.about, name='about-page'),
+    path('about/', views.about, name='page'),
     path('', views.home, name='home'),
     path('articles/',include('articles.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
